c8a11773e5edd1b492a15e7820dd0b5c0aa48239
ZezinhoTwo <107373132+Zezinhotwo@users.noreply.github.com>
2026-02-22 05:00:24 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/akio/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,11 @@
+package com.akio.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	
+}
