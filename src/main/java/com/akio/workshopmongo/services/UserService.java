c7f2aa80311a88f75016d687545001660a35ba0c
ZezinhoTwo <107373132+Zezinhotwo@users.noreply.github.com>
2026-02-23 01:30:25 -0300

    Insercao de usuario com POST

--- src/main/java/com/akio/workshopmongo/services/UserService.java
+++ src/main/java/com/akio/workshopmongo/services/UserService.java
@@ -6,15 +6,23 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.akio.workshopmongo.config.Instantiation;
 import com.akio.workshopmongo.domain.User;
+import com.akio.workshopmongo.dto.UserDTO;
 import com.akio.workshopmongo.repository.UserRepository;
 import com.akio.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
 
+    private final Instantiation instantiation;
+
 	@Autowired
 	private UserRepository repo;
+
+    UserService(Instantiation instantiation) {
+        this.instantiation = instantiation;
+    }
 
 	public List<User> findAll() {
 		return repo.findAll();
@@ -25,4 +33,13 @@ public class UserService {
 
 		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto not found!" + id));
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(),objDto.getEmail());
+	}
+	
 }
